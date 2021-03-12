5cab84b4a430cc7d39ed9799f8d2fa251daa4fbe
Bruno Castro <brunohcastro@hotmail.com>
2021-03-11 21:57:16 -0300

    Update ledger mode

--- lisp/init-ledger.el
+++ lisp/init-ledger.el
@@ -13,13 +13,15 @@
     (define-key ledger-mode-map (kbd "C-o") 'open-line))
 
   (setq ledger-highlight-xact-under-point nil
-        ledger-use-iso-dates nil)
+        ledger-use-iso-dates t)
 
   (with-eval-after-load 'ledger-mode
     (when (memq window-system '(mac ns))
       (exec-path-from-shell-copy-env "LEDGER_FILE")))
 
-  (add-hook 'ledger-mode-hook 'goto-address-prog-mode))
+  (add-hook 'ledger-mode-hook 'goto-address-prog-mode)
+
+  (add-to-list 'auto-mode-alist '("\\.\\(dat\\|ledger\\)\\'" . ledger-mode)))
 
 (provide 'init-ledger)
 ;;; init-ledger.el ends here
